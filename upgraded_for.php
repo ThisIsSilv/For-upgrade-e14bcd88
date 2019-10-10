9bccadc4c886c9a1ac3d2cce3a35c4c19517fb55
Silvano Bakacak <2119823@talnet.nl>
2019-10-10 08:49:52 +0200

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,17 @@
+<?php
+$array = array(
+    "Kreeft\n",
+    "Bob\n",
+    "Klaas\n",
+    "Man\n",
+    "Geel\n",
+    "Driehoek\n",
+    "kwart voor drie\n",
+
+
+);
+
+foreach ($array as $value) {
+
+
+echo "$value";}
